99238808178000ba31b667860c53397fe9469971
Darien-Cepani <darien.cepani42@gmail.com>
2021-04-14 18:50:47 +0200

    Made timer not change on reload

--- src/app/order-form/order-form.component.ts
+++ src/app/order-form/order-form.component.ts
@@ -8,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class OrderFormComponent implements OnInit {
   oldPrice : number;
   newPrice : number;
+  strTime: number;
 
   constructor() {
     this.oldPrice = 9000;
     this.newPrice = 4500;
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    this.strTime = hours + minutes + seconds;
   }
 
   ngOnInit(): void {
