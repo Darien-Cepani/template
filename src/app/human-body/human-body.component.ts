cc2a2b76d9a8ab18c1294672634c00e433de87e7
Darien-Cepani <darien.cepani42@gmail.com>
2021-04-17 22:02:03 +0200

    Major design changes + added body parts diagram

--- src/app/human-body/human-body.component.ts
+++ src/app/human-body/human-body.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./human-body.component.scss'],
 })
 export class HumanBodyComponent implements OnInit {
-
+  public face = false;
+  public upper = false;
+  public lower = false;
+  public arms = false;
+  public legs = false;
   constructor() { }
 
   ngOnInit(): void {
